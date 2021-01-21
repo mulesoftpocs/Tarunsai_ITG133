652becabee43f7eef03ec47fa456e2fed7431f56
mulesoftpocs <77617770+mulesoftpocs@users.noreply.github.com>
2021-01-21 16:19:39 +0530

    Add files via upload

---
+++ poco/src/main/java/poco/Promo.java
@@ -0,0 +1,8 @@
+package poco;
+
+public class Promo {
+	public static String  col(int a,int b)
+	{
+		return(a+"x"+b+"="+a*b);
+	}
+}
---
+++ poco/src/main/java/poco/poco.java
@@ -0,0 +1,9 @@
+package poco;
+
+public class poco {
+	public static String  col(int a,int b)
+	{
+		return(a+"x"+b+"="+a*b);
+	}
+
+}
