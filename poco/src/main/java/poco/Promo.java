86db08cb159c85addd0968f4dd2d83f139e9cc97
mulesoftpocs <77617770+mulesoftpocs@users.noreply.github.com>
2021-01-22 13:14:59 +0530

    Delete poco directory

--- poco/src/main/java/poco/Promo.java
+++
@@ -1,8 +0,0 @@
-package poco;
-
-public class Promo {
-	public static String  col(int a,int b)
-	{
-		return(a+"x"+b+"="+a*b);
-	}
-}
--- poco/src/main/java/poco/poco.java
+++
@@ -1,9 +0,0 @@
-package poco;
-
-public class poco {
-	public static String  col(int a,int b)
-	{
-		return(a+"x"+b+"="+a*b);
-	}
-
-}
